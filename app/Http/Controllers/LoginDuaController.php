194a846ed2e04b3205c76480ffbe62f6c2451855
Raihan Naufal <bundanci008@gmail.com>
2025-01-15 14:52:36 +0700

    pit login

---
+++ app/Http/Controllers/LoginDuaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginDuaController extends Controller
+{
+    // 
+    public function sayHello(){
+        return "hello world";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginDuaController;
+use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/dua',[LoginDuaController::class, 'sayHello']);
+
+Route::get('/dashboard',[DashboardController::class, 'index']);
