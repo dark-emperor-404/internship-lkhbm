3fc98069d60787dea43aea2d4067b19bdc399569
Raihan Naufal <bundanci008@gmail.com>
2025-02-17 09:23:37 +0700

    feat Implmentation of Router Name

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,22 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/signup', function () {
-    return view('signup');
-});
+// Route::get('/signup', function () {
+//     return view('signup');
+// });
 
-Route::get('/loading', function () {
-    return view('loading');
-});
+// Route::get('/loading', function () {
+//     return view('loading');
+// });
 
 // Route::get('/dua',[LoginDuaController::class, 'sayHello']);
 
-Route::get('/dashboard',[DashboardController::class, 'index']);
+Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/user',[UserController::class, 'index']);
-Route::get('/user/tambah',[UserController::class, 'create']);
-Route::post('/user/simpan', [UserController::class, 'store']);
-Route::get('/user/{id}/show', [UserController::class, 'show']);
-Route::post('/user/{id}/update', [UserController::class, 'update']);
-Route::get('/user/{id}/delete', [UserController::class, 'destroy']);
+// user
+Route::get('/user',[UserController::class, 'index'])->name('user.index');
+Route::get('/user/tambah',[UserController::class, 'create'])->name('user.tambah');
+Route::post('/user/simpan', [UserController::class, 'store'])->name('user.simpan');
+Route::get('/user/{id}/show', [UserController::class, 'show'])->name('user.show');
+Route::post('/user/{id}/update', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/{id}/delete', [UserController::class, 'destroy'])->name('user.delete');
